6aef4f749897cf293d9fa1de28cf76e381e73a46
Kronos-exe <mishakirillov36@gmail.com>
2019-11-05 23:05:53 +0300

    first commit

---
+++ client.js
@@ -0,0 +1,31 @@
+const status = document.getElementById("status");
+const messages = document.getElementById("messages");
+const form = document.getElementById("form");
+const message = document.getElementById("message");
+
+
+const ws = new WebSocket("ws://localhost:3000");
+
+function setStatus(value) {
+    status.innerHTML = value;
+}
+
+function printMessage(value) {
+    const li = document.createElement("li");
+
+    li.innerHTML = value;
+    li.className = "list-group-item";
+
+    messages.appendChild(li);
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    ws.send(message.value);
+    message.value = "";
+});
+
+ws.onopen = () => setStatus("ONLINE");
+ws.onclose = () => setStatus("DISCONNECTED");
+ws.onmessage = (response) => printMessage(response.data);
